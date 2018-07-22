931cff9448710dca4626190cf0a983ca7f245cef
周志衠 <jhihjhun@gmail.com>
2018-07-23 02:15:03 +0800

    加入Net Standard Library專案

---
+++ NetStandardLibrary/MyTool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetStandardLibrary
+{
+    public class MyTool
+    {
+        public string ReturnString(string str)
+        {
+            return str;
+        }
+    }
+}
